93f34207d0a70a0654dfe8ca5c404d6928aa222f
Saksham Bhatt <sakshambhatt@192.168.1.3>
2023-10-24 19:46:17 +0530

    poc: installed and config react-native-esc-pos-printer

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -6,11 +6,62 @@ import {
   Text,
   Pressable,
 } from 'react-native';
+import EscPosPrinter, {
+  IPrinter,
+  getPrinterSeriesByName,
+} from 'react-native-esc-pos-printer';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
+  const [printer, setPrinter] = useState<IPrinter | null>(null);
+  const discoverPrinterPress = async () => {
+    try {
+      const printers = await EscPosPrinter.discover();
+      console.log({printers});
+      if (printers[0]) {
+        setPrinter(printers[0]);
+      }
+    } catch (e) {
+      console.log('error while discovering printer', JSON.stringify(e));
+    }
+  };
+
+  const printPress = async () => {
+    try {
+      if (printer !== null) {
+        await EscPosPrinter.init({
+          target: printer.target,
+          seriesName: getPrinterSeriesByName(printer.name),
+          language: 'EPOS2_LANG_EN',
+        });
+
+        const printing = new EscPosPrinter.printing();
+
+        const status = await printing
+          .initialize()
+          .align('center')
+          .size(3, 3)
+          .line('DUDE!')
+          .smooth(true)
+          .line('DUDE!')
+          .smooth(false)
+          .size(1, 1)
+          .text('is that a ')
+          .bold()
+          .underline()
+          .text('printer?')
+          .newline()
+          .cut()
+          .send();
+
+        console.log('Success:', status);
+      }
+    } catch (e) {
+      console.log('error while printing', JSON.stringify(e));
+    }
+  };
 
   return (
     <SafeAreaView
@@ -20,11 +71,11 @@ function App(): JSX.Element {
       ]}>
       <Text>Print test</Text>
 
-      <Pressable style={styles.button}>
+      <Pressable style={styles.button} onPress={discoverPrinterPress}>
         <Text style={styles.btnText}>Discover printer</Text>
       </Pressable>
 
-      <Pressable style={styles.button}>
+      <Pressable style={styles.button} onPress={printPress}>
         <Text style={styles.btnText}>Print</Text>
       </Pressable>
     </SafeAreaView>
